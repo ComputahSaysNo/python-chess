2d1b04dccfbe44934d7ea716dea78e35b30a6343
ComputahSaysNo <kevinmathew2002@gmail.com>
2017-02-09 00:11:31 +0000

    updates 8/2

--- main.py
+++ main.py
@@ -4,11 +4,20 @@ from display import *
 def main():
     board = Board()
     print_board_text(board)
+    current_player = WHITE
     while True:
+        print("It's " + current_player + "'s turn")
         move = str(input("Enter the start position and the end position of the move: ")).lower().split()
+        if board.get_piece(move[0]).colour != current_player:
+            print("That's not your piece")
+            continue
         try:
             board.make_move(move[0],move[1])
             print_board_text(board)
+            if current_player == WHITE:
+                current_player = BLACK
+            else:
+                current_player = WHITE
         except InvalidMoveError:
             print("Invalid move, try again")
 
